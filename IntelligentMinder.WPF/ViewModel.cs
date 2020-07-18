e7beeb9bbfd4f62e776ed7f38ae576d9f22e5655
gian-te <gian.te@outlook.com>
2020-07-19 01:56:08 +0800

    added csproj

---
+++ IntelligentMinder.WPF/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligentMinder.WPF
+{
+    /// <summary>
+    /// Load the Entities and put some logic here
+    /// </summary>
+    public class ViewModel
+    {
+    }
+}
